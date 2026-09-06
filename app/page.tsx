94a1c5d0c73351cdf8b2c8e15b9cf5b7f43216cc
zero-route <iostream911@gmail.com>
2026-09-06 17:44:49 +0800

    update page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import BlackHoleCanvas from "@/components/BlackHoleCanvas";
+
+export default function Home() {
+  return (
+    <main style={{ width: "100vw", height: "100vh" }}>
+      <BlackHoleCanvas />
+    </main>
+  );
+}
